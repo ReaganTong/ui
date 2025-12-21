845d489755e6b47a68e0c0e9ac8f2b1d9d6f8021
reagan1133 <reagantong@hotmail.com>
2025-12-21 23:21:41 +0800

    s

--- mobile app webiste/js/config.js
+++ mobile app webiste/js/config.js
@@ -1,4 +1,4 @@
 // js/config.js
-const SUPABASE_URL = 'https://your-project-id.supabase.co';
-const SUPABASE_KEY = 'your-anon-public-key';
+const SUPABASE_URL = 'https://yacthiglkcipbltixron.supabase.co';
+const SUPABASE_KEY = 'sb_publishable_thHRqeO7TmSOhuWj9TgB4A_cAw4BwXB';
 const _supabase = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
